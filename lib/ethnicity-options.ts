2dd69f70c0ae9fb572d32897d99d3a6b17a11898
diana klatt <46638737+dianaklatt@users.noreply.github.com>
2025-03-08 18:44:31 -0500

    Create ethnicity-options.ts

---
+++ lib/ethnicity-options.ts
@@ -0,0 +1,107 @@
+// Broad ethnicity groups
+export const ethnicityGroups = [
+  "East Asian",
+  "Southeast Asian",
+  "South Asian",
+  "Central Asian",
+  "West Asian/Middle Eastern",
+  "North Asian",
+  "Asian American",
+  "Asian Canadian",
+  "Asian Australian",
+  "Asian European",
+  "Mixed Asian Heritage",
+  "Pacific Islander",
+]
+
+// Specific ethnicities
+export const specificEthnicities = [
+  // East Asian
+  "Chinese",
+  "Japanese",
+  "Korean",
+  "Taiwanese",
+  "Mongolian",
+  "Tibetan",
+  "Hong Konger",
+
+  // Southeast Asian
+  "Filipino",
+  "Vietnamese",
+  "Thai",
+  "Indonesian",
+  "Malaysian",
+  "Singaporean",
+  "Cambodian",
+  "Laotian",
+  "Burmese",
+  "Hmong",
+  "Karen",
+
+  // South Asian
+  "Indian",
+  "Pakistani",
+  "Bangladeshi",
+  "Sri Lankan",
+  "Nepali",
+  "Bhutanese",
+  "Maldivian",
+  "Indo-Caribbean",
+  "Indo-Fijian",
+  "Tamil",
+  "Punjabi",
+  "Bengali",
+  "Gujarati",
+  "Kashmiri",
+  "Sindhi",
+  "Marathi",
+
+  // Central Asian
+  "Kazakh",
+  "Uzbek",
+  "Kyrgyz",
+  "Tajik",
+  "Turkmen",
+  "Afghan",
+  "Uyghur",
+
+  // West Asian/Middle Eastern
+  "Iranian/Persian",
+  "Iraqi",
+  "Lebanese",
+  "Syrian",
+  "Jordanian",
+  "Palestinian",
+  "Saudi",
+  "Yemeni",
+  "Omani",
+  "Emirati",
+  "Qatari",
+  "Bahraini",
+  "Kuwaiti",
+  "Turkish",
+
+  // North Asian
+  "Siberian",
+  "Russian Asian",
+  "Yakut",
+  "Tuvan",
+
+  // Pacific Islander
+  "Hawaiian",
+  "Samoan",
+  "Tongan",
+  "Fijian",
+  "Marshallese",
+  "Palauan",
+  "Chamorro",
+  "Tahitian",
+  "Maori",
+  "Micronesian",
+  "Melanesian",
+  "Polynesian",
+]
+
+// Combined list for dropdown
+export const allEthnicityOptions = [...ethnicityGroups, ...specificEthnicities]
+
